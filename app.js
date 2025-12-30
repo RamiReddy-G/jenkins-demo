e53fedd73a7fddfe897aef192b005b00c67e236f
ramireddy-G <ramireddygutti17@gmail.com>
2025-12-30 10:05:59 +0530

    _10

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from Jenkins + Docker this is a zero down time shifting');
+  res.end('Hello from Jenkins + Docker this is a zero down time shifting !');
 });
 
 server.listen(PORT, () => {
