622aa4b08762896e2de38bb38b53eb3e328600aa
ramireddy-G <ramireddygutti17@gmail.com>
2026-05-13 22:38:12 +0530

    changed the jenkins file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from Jenkins + Docker 🚀 Blue-Green Deployment Working!");
+  res.end("Hello from Jenkins + Docker 🚀 Blue-Green Deployment Working! this is the updated version");
 });
 //webhook
 server.listen(3000, () => {
